7f4875f1f084bc247fb6c9ecdbc3b6f7cdddb861
Alexander Smirnov <35576482+olekzonder@users.noreply.github.com>
2021-11-03 14:04:33 +0300

    add files

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dvg1 = new System.Windows.Forms.Timer(this.components);
+            this.dvg2 = new System.Windows.Forms.Timer(this.components);
+            this.dvg3 = new System.Windows.Forms.Timer(this.components);
+            this.stop1 = new System.Windows.Forms.Timer(this.components);
+            this.stop2 = new System.Windows.Forms.Timer(this.components);
+            this.stop3 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(50, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Погнали!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(154, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(311, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(451, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            this.label3.Visible = false;
+            // 
+            // dvg1
+            // 
+            this.dvg1.Interval = 25;
+            this.dvg1.Tick += new System.EventHandler(this.dvg1_Tick);
+            // 
+            // dvg2
+            // 
+            this.dvg2.Interval = 25;
+            this.dvg2.Tick += new System.EventHandler(this.dvg2_Tick);
+            // 
+            // dvg3
+            // 
+            this.dvg3.Interval = 25;
+            this.dvg3.Tick += new System.EventHandler(this.dvg3_Tick);
+            // 
+            // stop1
+            // 
+            this.stop1.Interval = 1500;
+            this.stop1.Tick += new System.EventHandler(this.stop1_Tick);
+            // 
+            // stop2
+            // 
+            this.stop2.Interval = 2500;
+            this.stop2.Tick += new System.EventHandler(this.stop2_Tick);
+            // 
+            // stop3
+            // 
+            this.stop3.Interval = 3500;
+            this.stop3.Tick += new System.EventHandler(this.stop3_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(310, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Баланс:  $100";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(454, 360);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 26);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(310, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Выбрать ставку: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(310, 398);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Осталось попыток: 0";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(50, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 56);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Сделать ставку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(5, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(593, 86);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Однорукий Бандит";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SlotMachine.Properties.Resources._default;
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(402, 115);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SlotMachine.Properties.Resources._default;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(263, 115);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::SlotMachine.Properties.Resources._default;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(111, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(604, 462);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Однорукий Бандит";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer dvg1;
+        private System.Windows.Forms.Timer dvg2;
+        private System.Windows.Forms.Timer dvg3;
+        private System.Windows.Forms.Timer stop1;
+        private System.Windows.Forms.Timer stop2;
+        private System.Windows.Forms.Timer stop3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SlotMachine/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        Image apple = Properties.Resources.apple;
+        Image cherry = Properties.Resources.cherry;
+        Image grapes = Properties.Resources.grapes;
+        Image lemon = Properties.Resources.lemon;
+        Image orange = Properties.Resources.orange;
+        Image plum = Properties.Resources.plum;
+        Image melon = Properties.Resources.melon;
+        Image seven = Properties.Resources.seven;
+        int secret = 0;
+        int secret_enabled = 0;
+        SoundPlayer sound = new SoundPlayer();
+        SoundPlayer loop = new SoundPlayer();
+        int balance = 100; //Исходный баланс.
+        int counter_money = 0; //Текущий ставка.
+        int counter_try = 0; //Счетчик попыток.
+        int win_money = 0; //Выигранные деньги.
+        bool IsActive = true; //Активность кнопки "Погнали!"
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+        public void dvg1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random(); //Создаем экземпляр класса Random
+            int dvg = random.Next(8); // Получаем случайное число от 0-7
+            label1.Text = dvg.ToString(); //Выводим полученное число
+            switch (dvg)
+            {
+                case 0:
+                    pictureBox1.Image = apple;
+                    break;
+                case 1:
+                    pictureBox1.Image = cherry;
+                    break;
+                case 2:
+                    pictureBox1.Image = grapes;
+                    break;
+                case 3:
+                    pictureBox1.Image = lemon;
+                    break;
+                case 4:
+                    pictureBox1.Image = orange;
+                    break;
+                case 5:
+                    pictureBox1.Image = plum;
+                    break;
+                case 6:
+                    pictureBox1.Image = melon;
+                    break;
+                case 7:
+                    pictureBox1.Image = seven;
+                    break;
+            }
+        }
+        public void dvg2_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int dvg = random.Next(8);
+            label2.Text = dvg.ToString();
+            switch (dvg)
+            {
+                case 0:
+                    pictureBox2.Image = apple;
+                    break;
+                case 1:
+                    pictureBox2.Image = cherry;
+                    break;
+                case 2:
+                    pictureBox2.Image = grapes;
+                    break;
+                case 3:
+                    pictureBox2.Image = lemon;
+                    break;
+                case 4:
+                    pictureBox2.Image = orange;
+                    break;
+                case 5:
+                    pictureBox2.Image = plum;
+                    break;
+                case 6:
+                    pictureBox2.Image = melon;
+                    break;
+                case 7:
+                    pictureBox2.Image = seven;
+                    break;
+            }
+        }
+        public void dvg3_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int dvg = random.Next(8);
+            label3.Text = dvg.ToString();
+            switch (dvg)
+            {
+                case 0:
+                    pictureBox3.Image = apple;
+                    break;
+                case 1:
+                    pictureBox3.Image = cherry;
+                    break;
+                case 2:
+                    pictureBox3.Image = grapes;
+                    break;
+                case 3:
+                    pictureBox3.Image = lemon;
+                    break;
+                case 4:
+                    pictureBox3.Image = orange;
+                    break;
+                case 5:
+                    pictureBox3.Image = plum;
+                    break;
+                case 6:
+                    pictureBox3.Image = melon;
+                    break;
+                case 7:
+                    pictureBox3.Image = seven;
+                    break;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sound.Stream = Properties.Resources.play;
+            sound.PlaySync();
+            loop.Stream = Properties.Resources.tick;
+            loop.PlayLooping();
+            //активируем таймеры
+            dvg1.Enabled = true;
+            dvg2.Enabled = true;
+            dvg3.Enabled = true;
+            stop1.Enabled = true;
+            stop2.Enabled = true;
+            stop3.Enabled = true;
+            IsActive = true;
+            button1.Enabled = false; //Пока барабаны крутятся кнопка "Погнали!"  заблокирована.
+        }
+        private void stop1_Tick(object sender, EventArgs e)
+        {
+            dvg1.Enabled = false; //Останавливаем таймер запускающий первый барабан.
+            stop1.Enabled = false; //Останавливаем таймер останавливающий первый барабан.
+            if (secret_enabled == 1)
+            {
+                label1.Text = "7";
+                pictureBox1.Image = Properties.Resources.seven;
+            }
+        }
+        private void stop2_Tick(object sender, EventArgs e)
+        {
+            dvg2.Enabled = false;
+            stop2.Enabled = false;
+            if (secret_enabled == 1)
+            {
+                label2.Text = "7";
+                pictureBox2.Image = Properties.Resources.seven;
+            }
+        }
+        private void stop3_Tick(object sender, EventArgs e)
+        {
+            loop.Stop();
+            dvg3.Enabled = false;
+            stop3.Enabled = false;
+            if (secret_enabled == 1)
+            {
+                label3.Text = "7";
+                pictureBox3.Image = Properties.Resources.seven;
+            }
+            loop.Stop();
+            counter_try--; //Уменьшаем число попыток.
+            Win_Money();
+            if (IsActive)
+            {
+                if (counter_try != 0) //Если число попыток больше 0, то даем возможность нажать на кнопку "Погнали!" еще раз, если нет, то блокируем кнопку "Погнали!", и выводим информационное окно.
+                {
+                    label6.Text = "Осталось попыток: " + counter_try;
+                    button1.Enabled = true;
+                }
+                else
+                {
+                    label6.Text = "Осталось попыток: " + counter_try;
+                    MessageBox.Show("Делайте новую ставку!", "Попытки закончились...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    button2.Enabled = true;
+                }
+            }
+        }
+        private void Init_Counter(decimal counter)
+        {
+            counter_money = Convert.ToInt32(counter);
+            balance = balance - counter_money;
+            label4.Text = "Баланс: $" + balance;
+            counter_try = 5;
+            label6.Text = "Осталось попыток: " + counter_try;
+        }
+
+        private void Win_Money()
+        {
+            if (label1.Text == "0" && label2.Text == "0" && label3.Text == "0") Upd_Win_Money(17);
+            if (label1.Text == "1" && label2.Text == "1" && label3.Text == "1") Upd_Win_Money(10);
+            if (label1.Text == "2" && label2.Text == "2" && label3.Text == "2") Upd_Win_Money(11);
+            if (label1.Text == "3" && label2.Text == "3" && label3.Text == "3") Upd_Win_Money(12);
+            if (label1.Text == "4" && label2.Text == "4" && label3.Text == "4") Upd_Win_Money(13);
+            if (label1.Text == "5" && label2.Text == "5" && label3.Text == "5") Upd_Win_Money(14);
+            if (label1.Text == "6" && label2.Text == "6" && label3.Text == "6") Upd_Win_Money(15);
+            if (label1.Text == "7" && label2.Text == "7" && label3.Text == "7") Upd_Win_Money(20);
+            else
+            {
+                if ((label1.Text == "7" && label2.Text == "7") || (label2.Text == "7" && label3.Text == "7")) Upd_Win_Money(10);
+            }
+            if ((label1.Text == "0" && label2.Text == "0") || (label2.Text == "0" && label3.Text == "0")) Upd_Win_Money(7);
+            if ((label1.Text == "1" && label2.Text == "1") || (label2.Text == "1" && label3.Text == "1")) Upd_Win_Money(1);
+            if ((label1.Text == "2" && label2.Text == "2") || (label2.Text == "2" && label3.Text == "2")) Upd_Win_Money(2);
+            if ((label1.Text == "3" && label2.Text == "3") || (label2.Text == "3" && label3.Text == "3")) Upd_Win_Money(3);
+            if ((label1.Text == "4" && label2.Text == "4") || (label2.Text == "4" && label3.Text == "4")) Upd_Win_Money(4);
+            if ((label1.Text == "5" && label2.Text == "5") || (label2.Text == "5" && label3.Text == "5")) Upd_Win_Money(5);
+            if ((label1.Text == "6" && label2.Text == "6") || (label2.Text == "6" && label3.Text == "6")) Upd_Win_Money(6);
+        }
+        private void Upd_Win_Money(int number)
+        {
+            if (number == 20)
+            {
+                sound.Stream = Properties.Resources.jackpot;
+                sound.Play();
+                DialogResult jackpot = MessageBox.Show("Вы выиграли: автомат по языкам программирования", "Джекпот!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                balance += 10000;
+                label4.Text = "Баланс: $" + balance; //Выводим обновленный балансе
+                button1.Enabled = false; //Блокируем кнопку "Погнали!"
+                button2.Enabled = true; //Открываем кнопку "Сделать ставку"
+                IsActive = false; //Это костыль, может кто-то предложит как от него отказаться ))
+                if (jackpot == DialogResult.OK)
+                {
+                    MessageBox.Show("Делайте новую ставку!", "Новая игра", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    label6.Text = "Осталось попыток: 0"; // Скидываем оставшиеся попытки.
+                }
+            }
+            else
+            {
+                win_money = counter_money * number; //умножаем ставку на коэффициент получаем кол-во выигранных денег
+                sound.Stream = Properties.Resources.gotmoney;
+                sound.PlaySync();
+                DialogResult result = MessageBox.Show("Вы выиграли: $" + win_money, "Поздравляем!", MessageBoxButtons.OK, MessageBoxIcon.Warning); //Выыодим поздравления.
+                balance = balance + win_money; //Прибавляем выигрыш к балансу
+                label4.Text = "Баланс: $" + balance; //Выводим обновленный балансе
+                button1.Enabled = false; //Блокируем кнопку "Погнали!"
+                button2.Enabled = true; //Открываем кнопку "Сделать ставку"
+                IsActive = false; //Это костыль, может кто-то предложит как от него отказаться ))
+                if (result == DialogResult.OK)
+                {
+                    MessageBox.Show("Делайте новую ставку!", "Новая игра", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    label6.Text = "Осталось попыток: 0"; // Скидываем оставшиеся попытки.
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sound.Stream = Properties.Resources.inserted;
+            sound.PlaySync();
+            Init_Counter(numericUpDown1.Value);
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (secret < 10) secret++;
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (secret >= 10 && secret < 15) secret++;
+            if (secret == 15)
+            {
+                sound.Stream = Properties.Resources.secret;
+                sound.PlaySync();
+                secret_enabled = 1;
+                secret++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
